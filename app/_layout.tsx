3d0a2f88967d7a0951fce265efbaf33abb1ab34d
diegoezequielguillen <diegoezequielguillen@gmail.com>
2024-12-02 21:14:56 +0000

    feat (storybook): add Storybook to react native expo router project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
+function RootLayout() {
   return <Stack />;
 }
+
+let AppEntryPoint = RootLayout;
+
+const storybookEnabled = process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === "true";
+if (storybookEnabled) {
+  AppEntryPoint = require("../.storybook").default;
+}
+
+export default AppEntryPoint;
--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World</Text>
+      <Text>Hello World 4</Text>
     </View>
   );
 }
