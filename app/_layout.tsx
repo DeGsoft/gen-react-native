a1634173950a7f47167e9d670d845e68db35542e
diegoezequielguillen <diegoezequielguillen@gmail.com>
2024-12-04 00:00:04 +0000

    feat (tinybase): add TinyBase to react native project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,41 @@
 import { Stack } from "expo-router";
+import { Platform, StyleSheet } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { createStore } from 'tinybase';
+import {
+  Provider,
+  useCreatePersister,
+  useCreateStore
+} from 'tinybase/ui-react';
+
+const useAndStartPersister = (store) =>
+  // Persist store to Expo SQLite or local storage; load once, then auto-save.
+  useCreatePersister(
+    store,
+    (store) =>
+      Platform.OS === 'web'
+        ? require('tinybase/persisters/persister-indexed-db').createIndexedDbPersister(store, 'todos')
+        : require('tinybase/persisters/persister-expo-sqlite').createExpoSqlitePersister(store, require('expo-sqlite').openDatabaseSync('todos.db')),
+    [],
+    (persister) => persister.load().then(persister.startAutoSave)
+  );
 
 function RootLayout() {
-  return <Stack />;
+  // Initialize the (memoized) TinyBase store and persist it.
+  const store = useCreateStore(createStore);
+  store.setValues({ title: "TinyBase Example" });
+  useAndStartPersister(store);
+  return (
+    // Wrap the app in TinyBase context, so the store is default throughout and
+    // a SafeAreaProvider/SafeAreaView so it fits the screen.
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.container}>
+          <Stack />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Provider>
+  );
 }
 
 let AppEntryPoint = RootLayout;
@@ -12,3 +46,12 @@ if (storybookEnabled) {
 }
 
 export default AppEntryPoint;
+
+// Styles for the app.
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    flex: 1,
+    margin: 16,
+  },
+});
--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,91 @@
-import { Text, View } from "react-native";
+import { useState } from 'react';
+import {
+  FlatList, StyleSheet, Text, TextInput,
+  TouchableOpacity, View
+} from "react-native";
+import {
+  useAddRowCallback,
+  useDelTableCallback,
+  useHasTable,
+  useRow,
+  useSetCellCallback,
+  useSortedRowIds,
+  useValue
+} from 'tinybase/ui-react';
+
+// The TinyBase table contains the todos, with 'text' and 'done' cells.
+const TODO_TABLE = 'todo';
+const TEXT_CELL = 'text';
+const DONE_CELL = 'done';
+
+// Emojis to decorate each todo.
+const NOT_DONE_ICON = String.fromCodePoint('0x1F7E0');
+const DONE_ICON = String.fromCodePoint('0x2705');
+
+// The text input component to add a new todo.
+const NewTodo = () => {
+  const [text, setText] = useState('');
+  const handleSubmitEditing = useAddRowCallback(
+    TODO_TABLE,
+    ({ nativeEvent: { text } }) => {
+      setText('');
+      return { [TEXT_CELL]: text, [DONE_CELL]: false };
+    }
+  );
+  return (
+    <TextInput
+      value={text}
+      onChangeText={(text) => setText(text)}
+      onSubmitEditing={handleSubmitEditing}
+      placeholder='What do you want to do today?'
+      style={styles.input}
+    />
+  );
+};
+
+// A single todo component, either 'not done' or 'done': press to toggle.
+const Todo = ({ id }) => {
+  const { text, done } = useRow(TODO_TABLE, id);
+  const handlePress = useSetCellCallback(
+    TODO_TABLE,
+    id,
+    DONE_CELL,
+    () => (done) => !done
+  );
+  return (
+    <TouchableOpacity
+      key={id}
+      onPress={handlePress}
+      style={[styles.todo, done ? styles.done : null]}
+    >
+      <Text style={styles.todoText}>
+        {done ? DONE_ICON : NOT_DONE_ICON} {text}
+      </Text>
+    </TouchableOpacity>
+  );
+};
+
+// A list component to show all the todos.
+const Todos = () => {
+  const renderItem = ({ item: id }) => <Todo id={id} />;
+  return (
+    <FlatList
+      data={useSortedRowIds(TODO_TABLE, DONE_CELL)}
+      renderItem={renderItem}
+      style={styles.todos}
+    />
+  );
+};
+
+// A button component to delete all the todos, only shows when there are some.
+const ClearTodos = () => {
+  const handlePress = useDelTableCallback(TODO_TABLE);
+  return useHasTable(TODO_TABLE) ? (
+    <TouchableOpacity onPress={handlePress}>
+      <Text style={styles.clearTodos}>Clear all</Text>
+    </TouchableOpacity>
+  ) : null;
+};
 
 export default function Index() {
   return (
@@ -9,7 +96,51 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Hello World 4</Text>
+      <Text style={styles.heading}>{useValue('title')}</Text>
+      <NewTodo />
+      <Todos />
+      <ClearTodos />
     </View>
   );
 }
+
+// Styles for the app.
+const styles = StyleSheet.create({
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  input: {
+    borderColor: '#999',
+    borderRadius: 8,
+    borderWidth: 2,
+    flex: 0,
+    height: 64,
+    marginTop: 16,
+    padding: 16,
+    fontSize: 20,
+  },
+  todos: {
+    flex: 1,
+    marginTop: 16,
+  },
+  todo: {
+    borderRadius: 8,
+    marginBottom: 16,
+    padding: 16,
+    backgroundColor: '#ffd',
+  },
+  done: {
+    backgroundColor: '#dfd',
+  },
+  todoText: {
+    fontSize: 20,
+  },
+  clearTodos: {
+    margin: 16,
+    flex: 0,
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
