df4632114b0f85084eb0a1c07e6fd0f4e18ab446
DeGsoft <diegoezequielguillen@gmail.com>
2025-06-12 10:07:46 -0300

    feat (utils): add secure store for store secrets

--- utils.ts
+++ utils.ts
@@ -1,12 +1,13 @@
 import * as Crypto from 'expo-crypto';
 import * as Linking from "expo-linking";
 import ExpoLocalization from "expo-localization/src/ExpoLocalization";
+import * as SecureStore from 'expo-secure-store';
 
 export const getUUIDv4 = () => Crypto.randomUUID();
 
-export const getDate = () =>  new Date().toISOString();
+export const getDate = () => new Date().toISOString();
 
-export const cleanPhoneNumber = (phoneNumber:string) =>  String(Number(phoneNumber.replace(/[^a-zA-Z0-9]/g, '')));
+export const cleanPhoneNumber = (phoneNumber: string) => String(Number(phoneNumber.replace(/[^a-zA-Z0-9]/g, '')));
 
 export const sendWhatsapp = (phoneNumber: number, message: string) => {
     const text = encodeURIComponent(message);
@@ -19,4 +20,11 @@ export const sendWhatsapp = (phoneNumber: number, message: string) => {
 
 export const getRegionCode = () => ExpoLocalization.getLocales()[0].regionCode || 'US';
 
-export const getCustomerCode = (regionCode)  => regionCode == 'AR' ? 'C' : '0';
+export const getCustomerCode = (regionCode) => regionCode == 'AR' ? 'C' : '0';
+
+export const saveSecureStore = async (key: string, value: string) => {
+    await SecureStore.setItemAsync(key, value);
+}
+
+export const getValueForSecureStore = async (key: string) =>
+    await SecureStore.getItemAsync(key);
